b728b66e68f701cf4f61d822a0a9c1dc068d87cd
yucheng0 <yucheng0939@gmail.com>
2020-08-30 22:12:06 +0800

    init

---
+++ app/src/main/java/com/example/texttospeech/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.texttospeech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    //Text To Speech
+    lateinit var mTTS:TextToSpeech
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mTTS = TextToSpeech(applicationContext, TextToSpeech.OnInitListener { status ->
+            if (status != TextToSpeech.ERROR){
+                //if there is no error then set language
+                mTTS.language = Locale.UK
+            }
+        })
+
+        //speak button click
+        speakBtn.setOnClickListener {
+            //get text from edit text
+            val toSpeak = textEt.text.toString()
+            if (toSpeak == ""){
+                //if there is no text in edit text
+                Toast.makeText(this, "Enter text", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                //if there is text in edit text
+                Toast.makeText(this, toSpeak, Toast.LENGTH_SHORT).show()
+                mTTS.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null)
+            }
+        }
+
+        //stop speaking button click
+        stopBtn.setOnClickListener {
+            if (mTTS.isSpeaking){
+                //if speaking then stop
+                mTTS.stop()
+                //mTTS.shutdown()
+            }
+            else{
+                //if not speaking
+                Toast.makeText(this, "Not speaking", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onPause() {
+        if (mTTS.isSpeaking){
+            //if speaking then stop
+            mTTS.stop()
+            //mTTS.shutdown()
+        }
+        super.onPause()
+    }
+}
